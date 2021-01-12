f757508e4084397672114010b14f9639fd6b4e6d
Mizraelko <vkoporov@gmail.com>
2021-01-12 13:07:39 +0300

    style app

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+import './todo-list-item.css';
+
+const TodoListItem = ({ label, important = false }) => {
+
+  const [done, setDone] = useState(false)
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal',
+    textDecoration: done ?  'line-through' : '',
+  };
+
+  return (
+    <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={style}
+        onClick={() => {setDone(!done)}}
+        >
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+};
+
+export default TodoListItem;
